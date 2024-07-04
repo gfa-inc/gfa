a696b762bd91e291a2b2a6861c4127c9d43e94dc
jack li <jackli1@shein.com>
2024-07-04 16:27:07 +0800

    feat: add session package

---
+++ middlewares/session/session.go
@@ -0,0 +1,65 @@
+package session
+
+import (
+	"github.com/gfa-inc/gfa/common/config"
+	"github.com/gfa-inc/gfa/common/logger"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var DefaultTimeout = 86400
+
+type Config struct {
+	PrivateKey string
+	MaxAge     int
+	Domain     string
+	Redis      RedisConfig
+}
+
+type RedisConfig struct {
+	MaxIdleConnSize int
+	Network         string
+	Addrs           []string
+	Password        string
+}
+
+func Session() gin.HandlerFunc {
+	option := Config{
+		MaxAge: DefaultTimeout,
+		Redis: RedisConfig{
+			MaxIdleConnSize: 10,
+			Network:         "tcp",
+		},
+	}
+	err := config.UnmarshalKey("session", &option)
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	if len(option.Redis.Addrs) == 0 {
+		logger.Panic("No session config found")
+	}
+	newRedisStore, err := redis.NewStore(option.Redis.MaxIdleConnSize, option.Redis.Network, option.Redis.Addrs[0],
+		option.Redis.Password, []byte(option.PrivateKey))
+	if err != nil {
+		logger.Panic(err)
+	}
+
+	newRedisStore.Options(sessions.Options{
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteLaxMode,
+		MaxAge:   option.MaxAge,
+		Domain:   option.Domain,
+	})
+
+	logger.Info("Use session middleware")
+	return sessions.Sessions("_SESSIONID", newRedisStore)
+}
+
+func Enabled() bool {
+	return config.Get("session") != nil
+}
