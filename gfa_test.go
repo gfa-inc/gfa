9e2978ce298382e7d9deefc215ddf20e6a1cbc2f
jack li <jackli1@shein.com>
2024-08-30 14:01:57 +0800

    feat: test json core logger

--- gfa_test.go
+++ gfa_test.go
@@ -1,12 +1,16 @@
 package gfa
 
 import (
+	"context"
 	"github.com/gfa-inc/gfa/common/db/mysqlx"
 	"github.com/gfa-inc/gfa/common/logger"
+	"github.com/gfa-inc/gfa/common/mq/kafkax"
 	"github.com/gfa-inc/gfa/core"
 	"github.com/gfa-inc/gfa/middlewares/security"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
+	"go.uber.org/zap/zapcore"
 	"testing"
 )
 
@@ -45,6 +49,28 @@ func (tc *testController) Setup(r *gin.RouterGroup) {
 }
 
 func TestRun(t *testing.T) {
+	logger.RegisterCore("json", logger.NewJsonCore(logger.NewJsonCoreWriter(func(b []byte) (int, error) {
+		if kafkax.ProducerClient == nil {
+			return 0, nil
+		}
+
+		msg := make([]byte, len(b))
+		copy(msg, b)
+		err := kafkax.ProducerClient.WriteMessages(context.Background(), kafka.Message{
+			Value: msg,
+		})
+		if err != nil {
+			return 0, err
+		}
+
+		return len(b), nil
+	}), func(cfg *zapcore.EncoderConfig) {
+		cfg.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02T15:04:05Z0700")
+		cfg.TimeKey = "timestamp"
+		cfg.MessageKey = "message"
+		cfg.CallerKey = "logger"
+	}))
+
 	Default()
 
 	AddController(&testController{})
