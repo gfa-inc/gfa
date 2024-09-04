079b562e40d244bc5ec35cce1c82d5bd4b238d98
jack li <jackli1@shein.com>
2024-09-04 20:43:45 +0800

    feat: log clientIP

--- middlewares/access_log/access_log.go
+++ middlewares/access_log/access_log.go
@@ -1,33 +1,52 @@
-package middlewares
+package access_log
 
 import (
+	"fmt"
 	"github.com/gfa-inc/gfa/common/logger"
-	"github.com/gin-contrib/requestid"
+	"github.com/gfa-inc/gfa/utils/router"
 	"github.com/gin-gonic/gin"
 	"time"
 )
 
+var (
+	Matcher     *router.Matcher
+	ClientIPKey = "clientIP"
+	LatencyKey  = "latency"
+)
+
 func AccessLog() gin.HandlerFunc {
+	Matcher = router.New()
+
+	logger.AddContextKey(ClientIPKey)
+	logger.AddContextKey(LatencyKey)
+
 	logger.Info("Access log middleware enabled")
 	return func(c *gin.Context) {
 		start := time.Now()
+		// request method
+		method := c.Request.Method
 		// request path
 		path := c.Request.URL.Path
 		// client ip
 		clientIP := c.ClientIP()
-		// request method
-		method := c.Request.Method
-		// request id
-		requestID := requestid.Get(c)
+
+		c.Set(ClientIPKey, clientIP)
 
 		c.Next()
 
+		key := fmt.Sprintf("%s#%s", path, method)
+		if Matcher.Match(key) {
+			return
+		}
+
 		// request latency
 		latency := time.Now().Sub(start).Milliseconds()
+		c.Set(LatencyKey, latency)
 		// response status
 		status := c.Writer.Status()
 		// error message
 		errorMessage := c.Errors.ByType(gin.ErrorTypePrivate).String()
-		logger.Infof("%s [%d] %s %s %dms %s %s", clientIP, status, method, path, latency, requestID, errorMessage)
+
+		logger.TInfof(c, "[%d] %s %s %dms %s", status, method, path, latency, errorMessage)
 	}
 }
