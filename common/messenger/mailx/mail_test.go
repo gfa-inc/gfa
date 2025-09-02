d0629a3a37c91df942fda236989100db86d57c60
ypli0629 <iyapeng0629@gmail.com>
2025-09-02 18:19:26 +0800

    feat: add test

---
+++ common/messenger/mailx/mail_test.go
@@ -0,0 +1,16 @@
+package mailx
+
+import (
+	"testing"
+
+	"github.com/gfa-inc/gfa/common/config"
+	"github.com/gfa-inc/gfa/common/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetup(t *testing.T) {
+	config.Setup(config.WithPath("../../../"))
+	logger.Setup()
+	Setup()
+	assert.NotNil(t, Client)
+}
