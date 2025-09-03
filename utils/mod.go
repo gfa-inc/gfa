381247d5f044cdad251f6d664f2f1e1263bdb9dd
ypli0629 <iyapeng0629@gmail.com>
2025-09-03 19:22:18 +0800

    feat: add Test helper func

---
+++ utils/mod.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func FindModuleRoot(dir string) (roots string) {
+	if dir == "" {
+		panic("dir not set")
+	}
+	dir = filepath.Clean(dir)
+
+	// Look for enclosing go.mod.
+	for {
+		if fi, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil && !fi.IsDir() {
+			return dir
+		}
+		d := filepath.Dir(dir)
+		if d == dir {
+			break
+		}
+		dir = d
+	}
+	return ""
+}
